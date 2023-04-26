0a11ae508905c5c8ad85a2b3d6fe393e3c84a571
Marcinek <marcinbator.ofc@gmail.com>
2023-04-26 14:19:24 +0200

    fix for refactor

--- src/main/java/com/example/biblioter/DBConnect.java
+++ src/main/java/com/example/biblioter/DBConnect.java
@@ -23,30 +23,51 @@ public class DBConnect {
         connection = DriverManager.getConnection(url, username, password);
     }
 
+    
 
-    //DB connection
-
-    public boolean isClosed() throws SQLException {
-        return connection.isClosed();
+    public Book getBook(int id) throws SQLException {
+        Book book=new Book();
+        PreparedStatement statement=connection.prepareStatement("SELECT * FROM bookstable WHERE id=?");
+        statement.setInt(1,id);
+        return getBook(book, statement);
     }
-
-    public void close() throws SQLException {
-        connection.close();
+    public Book getBook(String title) throws SQLException {
+        Book book=new Book();
+        PreparedStatement statement=connection.prepareStatement("SELECT * FROM bookstable WHERE title=?");
+        statement.setString(1,title);
+        return getBook(book, statement);
     }
-
-
-    //DB utilities
-
-    private void downloadBook(ResultSet result, Book book) throws SQLException {
-        book.setId(result.getInt("id"));
-        book.setTitle(result.getString("title"));
-        book.setAuthor(result.getString("author"));
-        book.setCategory(result.getString("category"));
-        book.setBorrowed(result.getString("borrowed"));
-        book.setAccessible(result.getBoolean("accessibility"));
+    private Book getBook(Book book, PreparedStatement statement) throws SQLException {
+        ResultSet result = statement.executeQuery();
+        while(result.next()){
+            book.setId(result.getInt("id"));
+            book.setTitle(result.getString("title"));
+            book.setAuthor(result.getString("author"));
+            book.setCategory(result.getString("category"));
+            book.setBorrowed(result.getString("borrowed"));
+            book.setAccessible(result.getBoolean("accessibility"));
+        }
+        return book;
     }
-
-    private void uploadBook(PreparedStatement statement, Book book) throws SQLException {
+    public List<Book> getBooks() throws SQLException {
+        List<Book> books = new ArrayList<>();
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM bookstable");
+        ResultSet result=statement.executeQuery();
+        while(result.next()){
+            Book book=new Book();
+            book.setId(result.getInt("id"));
+            book.setTitle(result.getString("title"));
+            book.setAuthor(result.getString("author"));
+            book.setCategory(result.getString("category"));
+            book.setBorrowed(result.getString("borrowed"));
+            book.setAccessible(result.getBoolean("accessibility"));
+            books.add(book);
+            booksAmount++;
+        }
+        return books;
+    }
+    public void addBook(Book book) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("INSERT INTO bookstable (title, author, category, borrowed) VALUES(?, ?, ?, ?, ?)");
         statement.setString(1, book.getTitle());
         statement.setString(2, book.getAuthor());
         statement.setString(3, book.getCategory());
@@ -54,54 +75,25 @@ public class DBConnect {
         statement.setBoolean(5,book.isAccessible());
         statement.executeUpdate();
     }
-
-
-    //DB operations
-
-    public Book getBook(int id) throws SQLException {
-        Book book=new Book();
-        PreparedStatement statement=connection.prepareStatement("SELECT * FROM bookstable WHERE id=?");
-        statement.setInt(1,id);
-        ResultSet result = statement.executeQuery();
-        downloadBook(result, book);
-        return book;
-    }
-
-    public Book getBook(String title) throws SQLException {
-        Book book=new Book();
-        PreparedStatement statement=connection.prepareStatement("SELECT * FROM bookstable WHERE title=?");
-        statement.setString(1,title);
-        ResultSet result = statement.executeQuery();
-        downloadBook(result, book);
-        return book;
-    }
-
-    public List<Book> getBooks() throws SQLException {
-        List<Book> books = new ArrayList<>();
-        PreparedStatement statement = connection.prepareStatement("SELECT * FROM bookstable");
-        ResultSet result=statement.executeQuery();
-        while(result.next()){
-            Book book=new Book();
-            downloadBook(result, book);
-            books.add(book);
-            booksAmount++;
-        }
-        return books;
-    }
-
-    public void addBook(Book book) throws SQLException {
-        PreparedStatement statement = connection.prepareStatement("INSERT INTO bookstable (title, author, category, borrowed, accessibility) VALUES(?, ?, ?, ?, ?)");
-        uploadBook(statement, book);
-    }
-
     public void editBook(Book book) throws SQLException{
         PreparedStatement statement = connection.prepareStatement("UPDATE bookstable SET title=?, author=?, category=?, borrowed=?, accessibility=? WHERE id=?");
-        uploadBook(statement, book);
+        statement.setString(1, book.getTitle());
+        statement.setString(2, book.getAuthor());
+        statement.setString(3, book.getCategory());
+        statement.setString(4, book.getBorrowed());
+        statement.setBoolean(5,book.isAccessible());
+        statement.setString(6, Integer.toString(book.getId()));
+        statement.executeUpdate();
     }
-
     public void deleteBook(Book book) throws SQLException {
         PreparedStatement statement=connection.prepareStatement("DELETE FROM bookstable WHERE id=?");
-        statement.setInt(1,book.getId());
+        statement.setString(1,Integer.toString(book.getId()));
         statement.executeUpdate();
+    }
+    public boolean isClosed() throws SQLException {
+        return connection.isClosed();
+    }
+    public void close() throws SQLException {
+        connection.close();
     }
 }
