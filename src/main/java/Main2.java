b2c950692dbc5c665db06ee87a38bc77ffc78171
Marcinek <marcinbator.ofc@gmail.com>
2023-05-09 22:08:54 +0200

    release

---
+++ src/main/java/Main2.java
@@ -0,0 +1,9 @@
+import service.BiblioTER;
+
+import java.io.IOException;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BiblioTER.main(args);
+    }
+}
